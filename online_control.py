0be76b7fde3abc1be68aaff21de88d887eba89e0
Thomas Gläßle <t_glaessle@gmx.de>
2014-01-14 05:57:56 +0100

    Fix low level API wrapper according to 2013 docs
    With the improved documentation of the BeamOptikDLL.dll from late 2013
    some of the questions regarding the API are now resolved.
    
    The docstrings were improved to follow sphinx conventions. This will
    make it easy to auto-generate documentation for this module.

--- online_control.py
+++ online_control.py
@@ -17,6 +17,21 @@ from collections import MutableMapping
 
 EFI = namedtuple('EFI', ['energy', 'focus', 'intensity', 'gantry_angle'])
 
+def enum(*sequential):
+    class Enum(int):
+        def __str__(self):
+            return sequential[int(self)]
+        def __repr__(self):
+            return '%s(%s=%d)' % (self.__class__.__name, self, int(self))
+    for i,v in enumerate(sequential):
+        setattr(Enum, v, i)
+    return Enum
+
+DVMStatus = enum('Stop', 'Idle', 'Init', 'Ready', 'Busy', 'Finish', 'Error')
+GetOptions = enum('Current', 'Saved')
+ExecOptions = enum('CalcAll', 'CalcDif', 'SimplyStore')
+GetSDOptions = enum('Current', 'Database', 'Test')
+
 def load_shared_library(shared_library_name = 'BeamOptikDLL'):
     """
     Load the shared library.
@@ -51,7 +66,20 @@ class BeamOptikDLL(object):
         self.lib = library
         self.iid = None
 
+    #----------------------------------------
+    # internal methods
+    #----------------------------------------
+
     def _check_return(self, done):
+        """
+        Check DLL-API exit code for errors and raise exception.
+
+        :param int done: exit code of an DLL function
+        :raises RuntimeError: if ``done != 0``
+
+        For internal use only!
+
+        """
         if done == 0:
             return
         errors = [
@@ -73,12 +101,13 @@ class BeamOptikDLL(object):
 
     def __call__(self, function, *params):
         """
-        Call the specified method.
+        Call the specified DLL function.
 
-        This is a low-level function that should only be used internally.
+        :param str function: name of the function to call
+        :param params: ctype function parameters except for piInstance and piDone.
+        :raises RuntimeError: if the exit code indicates any error
 
-        The params must neither include piInstance nor piDone. If an error
-        is returned from the library a RuntimeError will be raised.
+        For internal use only!
 
         """
         done = Int()
@@ -89,80 +118,138 @@ class BeamOptikDLL(object):
             params.insert(5, done)
         else:
             params.append(done)
-
         self.lib[function](*(ctypes.byref(param) for param in params))
         self._check_return(done.value)
 
+    #----------------------------------------
+    # public API
+    #----------------------------------------
+
     def GetInterfaceInstance(self):
-        """Call GetInterfaceInstance(). Returns instance_id."""
-        if self.iid.value is None:
-            self.iid = Int()
-            try:
-                self('GetInterfaceInstance')
-            except RuntimeError:
-                self.iid = None
-                raise
+        """
+        Connect to database and initialize DLL.
+
+        :return: new instance id
+        :rtype: int
+        :raises RuntimeError: if the exit code indicates any error
+
+        """
+        self.iid = Int()
+        try:
+            self('GetInterfaceInstance')
+        except RuntimeError:
+            self.iid = None
+            raise
         return self.iid.value
 
     def FreeInterfaceInstance(self):
-        """Call FreeInterfaceInstance()."""
-        if self.iid is not None:
-            self('FreeInterfaceInstance')
-            self.iid = None
+        """
+        Free resources.
+
+        :raises RuntimeError: if the exit code indicates any error
+
+        """
+        self('FreeInterfaceInstance')
+        self.iid = None
 
     def DisableMessageBoxes(self):
-        """Call DisableMessageBoxes()."""
+        """
+        Prevent creation of certain message boxes.
+
+        :raises RuntimeError: if the exit code indicates any error
+
+        """
         self('DisableMessageBoxes')
 
     def GetDVMStatus(self):
-        """Call GetDVMStatus(). Returns status."""
+        """
+        Get current status of selected virtual accelerator.
+
+        :return: DVM status
+        :rtype: DVMStatus
+        :raises RuntimeError: if the exit code indicates any error
+
+        """
         status = Int()
         self('GetDVMStatus', status)
-        return status.value
+        return DVMStatus(status.value)
 
     def SelectVAcc(self, vaccnum):
-        """Call SelectVAcc()."""
+        """
+        Select the virtual accelerator.
+
+        :param int vaccnum: virtual accelerator number (0-255)
+        :raises RuntimeError: if the exit code indicates any error
+
+        """
         self('SelectVAcc', Int(vaccnum))
 
     def SelectMEFI(self, vaccnum, channels):
         """
         Select MEFI combination.
 
-        :param int vaccnum: virtual accelerator number
+        :param int vaccnum: virtual accelerator number (0-255)
         :param EFI channels: EFI channel numbers
         :return: physical EFI values
         :rtype: EFI
-        :raises RuntimeError: if the function call fails
+        :raises RuntimeError: if the exit code indicates any error
 
         """
-        values = EFI(Double(), Double(), Double(), Double())
-        self('SelectMEFI', vaccnum, *(list(channels) + list(values)))
+        channels = [Int(c) for c in channels]
+        values = [Double(), Double(), Double(), Double()]
+        self('SelectMEFI', vaccnum, *(channels + values))
         return EFI(*[v.value for v in values])
 
     def GetSelectedVAcc(self):
-        """Call GetSelectedVAcc(). Returns vaccnum."""
+        """
+        Get selected virtual accelerator.
+
+        :return: virtual accelerator number
+        :rtype: int
+        :raises RuntimeError: if the exit code indicates any error
+
+        """
         vaccnum = Int()
         self('GetSelectedVAcc', vaccnum)
         return vaccnum.value
 
-    def GetFloatValue(self, name):
-        """Call GetFloatValue(). Returns value."""
-        # TODO: doc does not describe what options are possible and
-        # whether options is input or output
-        options = Int()
+    def GetFloatValue(self, name, options=GetOptions.Current):
+        """
+        Get parameter value.
+
+        :param str name: parameter name
+        :param GetOptions options: options
+        :return: parameter value
+        :rtype: float
+        :raises RuntimeError: if the exit code indicates any error
+
+        """
         value = Double()
-        self('GetFloatValue', Str(name), value, options)
+        self('GetFloatValue', Str(name), value, Int(options))
         return value.value
 
-    def SetFloatValue(self, name, value):
-        """Call SetFloatValue()."""
-        # TODO: doc does not describe what options are possible and
-        # whether options is input or output
-        options = Int()
-        self('SetFloatValue', Str(name), Double(value), options)
+    def SetFloatValue(self, name, value, options=0):
+        """
+        Set parameter value.
+
+        :param str name: parameter name
+        :param float value: parameter value
+        :param options: not used currently
+        :raises RuntimeError: if the exit code indicates any error
+
+        Changes take effect after calling :func:`ExecuteChanges`.
+
+        """
+        self('SetFloatValue', Str(name), Double(value), Int(options))
 
     def ExecuteChanges(self, options):
-        """Call ExecuteChanges()."""
+        """
+        Apply parameter changes.
+
+        :param ExecOptions options: what to do exactly
+        :raises RuntimeError: if the exit code indicates any error
+
+        """
         self('ExecuteChanges', Int(options))
 
     def SetNewValueCallback(self, callback):
@@ -171,36 +258,47 @@ class BeamOptikDLL(object):
         # TODO: howto create a python callback? Use Cython?
         raise NotImplementedError
 
-    def GetFloatValueSD(self, name):
-        """Call GetFloatValueSD(). Retuns value."""
-        # TODO: doc does not specify valid values for options
-        options = Int()
+    def GetFloatValueSD(self, name, options=0):
+        """
+        Get current beam measurement at specific element.
+
+        :param str name: parameter name (<observable>_<element name>)
+        :param GetSDOptions options: options
+        :return: index of observable
+        :rtype: int?
+
+        """
+        # TODO: either docs are still bad or this function is weird
         value = Double()
-        self('GetFloatValueSD', Str(name), value, options)
+        self('GetFloatValueSD', Str(name), value, Int(options))
         return value.value
 
-    def GetLastFloatValueSD(self, name):
-        """Call GetLastFloatValueSD(). Retuns value."""
+    def GetLastFloatValueSD(self, name, options=0):
+        """
+        Get previous beam measurement at specific element.
+
+        :param str name: parameter name (<observable>_<element name>)
+        :param GetSDOptions options: options
+        :return: index of observable
+        :raises RuntimeError: if the exit code indicates any error
+
+        """
         # TODO: doc does not specify valid values for options
-        options = Int()
         value = Double()
-        self('GetLastFloatValueSD', Str(name), value, options)
+        self('GetLastFloatValueSD', Str(name), value, Int(options))
         return value.value
 
     def StartRampDataGeneration(self, name):
         """Call StartRampDataGeneration(). Not implemented!"""
-        # TODO: doc cannot be more unclear.
-        raise NotImplementedError
+        raise NotImplementedError # TODO
 
     def GetRampDataValue(self, name):
         """Call GetRampDataValue(). Not implemented!"""
-        # TODO: doc cannot be more unclear.
-        raise NotImplementedError
+        raise NotImplementedError # TODO
 
     def SetIPC_DVM_ID(self, name):
         """Call SetIPC_DVM_ID(). Not implemented!"""
-        # TODO: doc cannot be more unclear.
-        raise NotImplementedError
+        raise NotImplementedError # TODO
 
     def GetMEFIValue(self)
         """
