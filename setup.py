0a87fdf09704a72cdc1cafa2f1c189d2d2689561
Thomas Gläßle <t_glaessle@gmx.de>
2015-03-04 17:21:32 +0100

    Define descriptive setup metadata in package

--- setup.py
+++ setup.py
@@ -1,16 +1,35 @@
 # encoding: utf-8
 from setuptools import setup
+from distutils.util import convert_path
+
+
+def read_file(path):
+    """Read a file in binary mode."""
+    with open(convert_path(path), 'rb') as f:
+        return f.read()
+
+
+def exec_file(path):
+    """Execute a python file and return the `globals` dictionary."""
+    namespace = {}
+    exec(read_file(path), namespace, namespace)
+    return namespace
+
+
+long_description = read_file('README.rst').decode('utf-8')
+meta = exec_file('hit/online_control/__init__.py')
+
 
 setup(
     name='hit_online_control',
-    version='0.0',
-    description='Online control for the HIT accelerator facility',
-    long_description=open('README.rst').read(),
-    author='Thomas Gläßle',
-    author_email='t_glaessle@gmx.de',
-    maintainer='Thomas Gläßle',
-    maintainer_email='t_glaessle@gmx.de',
-    url='https://bitbucket.org/coldfix/hit-online-control',
+    version=meta['__version__'],
+    description=meta['__summary__'],
+    long_description=long_description,
+    author=meta['__author__'],
+    author_email=meta['__email__'],
+    url=meta['__uri__'],
+    license=meta['__license__'],
+    classifiers=meta['__classifiers__'],
     packages=[
         'hit',
         'hit.online_control'
@@ -24,14 +43,4 @@ setup(
             'online_control = hit.online_control.madgui:Plugin'
         ]
     },
-    classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
-        'Intended Audience :: Healthcare Industry',
-        'Intended Audience :: Science/Research',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Topic :: Scientific/Engineering :: Medical Science Apps.',
-        'Topic :: Scientific/Engineering :: Physics' ],
-    license=None,
 )
