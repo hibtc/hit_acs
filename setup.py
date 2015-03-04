5b270e57ec7e1eaf5a33af8b814683b978aea528
Thomas Gläßle <t_glaessle@gmx.de>
2015-03-04 17:24:02 +0100

    Run setup only if invoked as main script

--- setup.py
+++ setup.py
@@ -16,31 +16,35 @@ def exec_file(path):
     return namespace
 
 
-long_description = read_file('README.rst').decode('utf-8')
-meta = exec_file('hit/online_control/__init__.py')
-
-
-setup(
-    name='hit_online_control',
-    version=meta['__version__'],
-    description=meta['__summary__'],
-    long_description=long_description,
-    author=meta['__author__'],
-    author_email=meta['__email__'],
-    url=meta['__uri__'],
-    license=meta['__license__'],
-    classifiers=meta['__classifiers__'],
-    packages=[
-        'hit',
-        'hit.online_control'
-    ],
-    namespace_packages=['hit'],
-    entry_points={
-        'gui_scripts': [
-            'online_control = hit.online_control.__main__:main'
+def main():
+    """Execute setup."""
+    long_description = read_file('README.rst').decode('utf-8')
+    meta = exec_file('hit/online_control/__init__.py')
+    setup(
+        name='hit_online_control',
+        version=meta['__version__'],
+        description=meta['__summary__'],
+        long_description=long_description,
+        author=meta['__author__'],
+        author_email=meta['__email__'],
+        url=meta['__uri__'],
+        license=meta['__license__'],
+        classifiers=meta['__classifiers__'],
+        packages=[
+            'hit',
+            'hit.online_control'
         ],
-        'madgui.widget.notebook.menu': [
-            'online_control = hit.online_control.madgui:Plugin'
-        ]
-    },
-)
+        namespace_packages=['hit'],
+        entry_points={
+            'gui_scripts': [
+                'online_control = hit.online_control.__main__:main'
+            ],
+            'madgui.widget.notebook.menu': [
+                'online_control = hit.online_control.madgui:Plugin'
+            ]
+        },
+    )
+
+
+if __name__ == '__main__':
+    main()
