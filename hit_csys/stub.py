f0d4042b45b1f61d8bba9eb34be9f41bf1a6d426
Thomas Gläßle <t_glaessle@gmx.de>
2018-07-06 22:52:48 +0200

    Mock the BeamOptikDLL wrapper class directly
    rather than imitating the ctypes proxy object.

--- hit_csys/stub.py
+++ hit_csys/stub.py
@@ -1,59 +1,35 @@
 """
-Stub class for BeamOptikDLL.dll ctypes proxy objects as used by
+Fake implementation of BeamOptikDLL wrapper. Emulates the API of
 :class:`~hit_csys.beamoptikdll.BeamOptikDLL`. Primarily used for
 offline testing of the basic functionality.
 """
 
 import logging
 import functools
-import ctypes
 import random
 
 from pydicti import dicti
 
-from .beamoptikdll import DVMStatus, _decode
-
-
-c_str = ctypes.c_char_p, ctypes.c_wchar_p
+from .beamoptikdll import DVMStatus, GetOptions, EFI
 
 
 __all__ = [
-    'BeamOptikDllProxy',
+    'BImpostikDLL',
 ]
 
 
-def _unbox(param):
-    """Unbox a call parameter created by ctypes.byref."""
-    return _decode(param.value) if isinstance(param, c_str) else param._obj
-
-
 def _api_meth(func):
-
-    """
-    Decorator for methods conforming to the BeamOptikDLL API.
-
-    Unboxes parameter references and sets the ``done`` from the function's
-    return value.
-    """
-
+    """Decorator for tracing calls to BeamOptikDLL API methods."""
     @functools.wraps(func)
     def wrapper(self, *args):
-        idone = 6 if func.__name__ == 'SelectMEFI' else len(args) - 1
-        done = _unbox(args[idone])
-        args = args[:idone] + args[idone+1:]
-        done.value = 0
-        unboxed_args = tuple(map(_unbox, args))
-        logging.debug('{}{}'.format(func.__name__, unboxed_args))
-        ret = func(self, *unboxed_args)
-        if ret is not None:
-            done.value = ret
-
+        logging.debug('{}{}'.format(func.__name__, args))
+        return func(self, *args)
     return wrapper
 
 
-class BeamOptikDllProxy(object):
+class BImpostikDLL(object):
 
-    """A fake implementation for a ctypes proxy of the BeamOptikDLL."""
+    """A fake implementation of the BeamOptikDLL wrapper."""
 
     # TODO: Support read-only/write-only parameters
     # TODO: Prevent writing unknown parameters by default
@@ -132,82 +108,75 @@ class BeamOptikDllProxy(object):
         pass
 
     @_api_meth
-    def GetInterfaceInstance(self, iid):
+    def GetInterfaceInstance(self):
         """Create a new interface instance."""
-        iid.value = 1337
         self.vacc = 1
         self.EFIA = (1, 1, 1, 1)
+        return 1337
 
     @_api_meth
-    def FreeInterfaceInstance(self, iid):
+    def FreeInterfaceInstance(self):
         """Destroy a previously created interface instance."""
         del self.vacc
         del self.EFIA
 
     @_api_meth
-    def GetDVMStatus(self, iid, status):
+    def GetDVMStatus(self, status):
         """Get DVM ready status."""
         # The test lib has no advanced status right now.
-        status.value = DVMStatus.Ready
+        return DVMStatus.Ready
 
     @_api_meth
-    def SelectVAcc(self, iid, vaccnum):
+    def SelectVAcc(self, vaccnum):
         """Set virtual accelerator number."""
-        self.vacc = vaccnum.value
+        self.vacc = vaccnum
 
     @_api_meth
-    def SelectMEFI(self, iid, vaccnum,
-                   energy, focus, intensity, gantry_angle,
-                   energy_val, focus_val, intensity_val, gantry_angle_val):
+    def SelectMEFI(self, vaccnum, energy, focus, intensity, gantry_angle=0):
         """Set MEFI in current VAcc."""
         # The real DLL requires SelectVAcc to be called in advance, so we
         # enforce this constraint here as well:
-        assert self.vacc == vaccnum.value
-        self.EFIA = (
-            energy.value,
-            focus.value,
-            intensity.value,
-            gantry_angle.value,
-        )
-        energy_val.value = float(energy.value)
-        focus_val.value = float(focus.value)
-        intensity_val.value = float(intensity.value)
-        gantry_angle_val.value = float(
-            self.params.get('gantry_angle', gantry_angle.value))
+        assert self.vacc == vaccnum
+        self.EFIA = (energy, focus, intensity, gantry_angle)
+        return EFI(
+            float(energy),
+            float(focus),
+            float(intensity),
+            float(self.params.get('gantry_angle', gantry_angle)))
 
     @_api_meth
-    def GetSelectedVAcc(self, iid, vaccnum):
+    def GetSelectedVAcc(self):
         """Get currently selected VAcc."""
-        vaccnum.value = self.vacc
+        return self.vacc
 
     @_api_meth
-    def GetFloatValue(self, iid, name, value, options):
+    def GetFloatValue(self, name, options=GetOptions.Current):
         """Get a float value from the "database"."""
-        value.value = float(self.params.get(name, 0))
+        return float(self.params.get(name, 0))
 
     @_api_meth
-    def SetFloatValue(self, iid, name, value, options):
+    def SetFloatValue(self, name, value, options=0):
         """Store a float value to the "database"."""
-        self.params[name] = value.value
+        self.params[name] = value
 
     @_api_meth
-    def ExecuteChanges(self, iid, options):
+    def ExecuteChanges(self, options):
         """Compute new measurements based on current model."""
         if self.auto_sd:
             self.update_sd_values(self.model())
 
     @_api_meth
-    def SetNewValueCallback(self, iid, callback):
+    def SetNewValueCallback(self, callback):
         """Not implemented."""
         raise NotImplementedError
 
     @_api_meth
-    def GetFloatValueSD(self, iid, name, value, options):
+    def GetFloatValueSD(self, name, options=0):
         """Get beam diagnostic value."""
         try:
-            value.value = self.sd_values[name] * 1000
+            return self.sd_values[name] * 1000
         except KeyError:
-            value.value = -9999.0
+            return -9999.0
 
     def update_sd_values(self, model):
         """Compute new measurements based on current model."""
@@ -233,43 +202,37 @@ class BeamOptikDllProxy(object):
                 })
 
     @_api_meth
-    def GetLastFloatValueSD(self, iid,
-                            name, value, vaccnum, options,
-                            energy, focus, intensity, gantry_angle):
+    def GetLastFloatValueSD(self, name, vaccnum,
+                            energy, focus, intensity, gantry_angle=0,
+                            options=0):
         """Get beam diagnostic value."""
         # behave exactly like GetFloatValueSD and ignore further parameters
         # for now
-        self.GetFloatValueSD.__wrapped__(self, iid, name, value, options)
+        return self.GetFloatValueSD(name, options)
 
     @_api_meth
-    def StartRampDataGeneration(self, iid,
-                                vaccnum, energy, focus, intensity, order_num):
+    def StartRampDataGeneration(self, vaccnum, energy, focus, intensity):
         """Not implemented."""
         raise NotImplementedError
 
     @_api_meth
-    def GetRampDataValue(self, iid, order_num, event_num, delay,
-                         parameter_name, device_name, value):
+    def GetRampDataValue(self, order_num, event_num, delay,
+                         parameter_name, device_name):
         """Not implemented."""
         raise NotImplementedError
 
     @_api_meth
-    def SetIPC_DVM_ID(self, iid, name):
+    def SetIPC_DVM_ID(self, name):
         """Not implemented."""
         raise NotImplementedError
 
     @_api_meth
-    def GetMEFIValue(self, iid,
-                     energy_val, focus_val, intensity_val, gantry_angle_val,
-                     energy_chn, focus_chn, intensity_chn, gantry_angle_chn):
+    def GetMEFIValue(self):
         """Get current MEFI combination."""
-        e, f, i, a = self.EFIA
-        energy_chn.value = e
-        focus_chn.value = f
-        intensity_chn.value = i
-        gantry_angle_chn.value = a
-        energy_val.value = float(energy_chn.value)
-        focus_val.value = float(focus_chn.value)
-        intensity_val.value = float(intensity_chn.value)
-        gantry_angle_val.value = float(
-            self.params.get('gantry_angle', gantry_angle_chn.value))
+        channels = EFI(*self.EFIA)
+        values = EFI(
+            float(channels.energy),
+            float(channels.focus),
+            float(channels.intensity),
+            float(self.params.get('gantry_angle', channels.gantry_angle)))
+        return (values, channels)
