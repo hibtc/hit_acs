7c4125d75775080ec4eba73a4d93a9dfc0fcb84c
Thomas Gläßle <t_glaessle@gmx.de>
2018-10-11 19:07:56 +0200

    Safeguard against invalid paths

--- hit_csys/stub.py
+++ hit_csys/stub.py
@@ -123,8 +123,8 @@ class BImpostikDLL(object):
             'jitter_interval': self.sd_cache.timeout,
             'auto_sd': self.auto_sd(),
             'auto_params': self.auto_params(),
-            'str_file': os.path.relpath(self.str_file),
-            'sd_file': os.path.relpath(self.sd_file),
+            'str_file': safe_relpath(self.str_file),
+            'sd_file': safe_relpath(self.sd_file),
         }
 
     def _toggle_jitter(self):
@@ -361,3 +361,10 @@ class BImpostikDLL(object):
             float(channels.intensity),
             float(self.params.get('gantry_angle', channels.gantry_angle)))
         return (values, channels)
+
+
+def safe_relpath(path, start=None):
+    try:
+        return path and os.path.relpath(path, start)
+    except ValueError:  # e.g. different drive on windows
+        return path
